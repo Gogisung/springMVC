257cace89250cca36e8fa3bb5e649f3e8eb6d369
Gogisung <ggs0707@naver.com>
2020-11-02 08:51:13 +0900

    project finally commit

--- src/kr/co/softcampus/dao/UserDao.java
+++ src/kr/co/softcampus/dao/UserDao.java
@@ -3,6 +3,7 @@ package kr.co.softcampus.dao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import kr.co.softcampus.beans.ContentBean;
 import kr.co.softcampus.beans.UserBean;
 import kr.co.softcampus.mapper.UserMapper;
 
@@ -31,5 +32,5 @@ public class UserDao {
 	public void modifyUserInfo(UserBean modifyUserBean) {
 		userMapper.modifyUserInfo(modifyUserBean);
 	}
-
+	
 }
